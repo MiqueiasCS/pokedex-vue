36193d450ec5558f2867870b5dd9f4d6b2e19226
Miqueias <miqs_carvalho@hotmail.com>
2022-10-26 21:22:19 -0300

    add placeholder ao input

--- src/components/InputSearch.vue
+++ src/components/InputSearch.vue
@@ -2,7 +2,12 @@
   <div class="search-container">
     <div class="input-container">
       <img src="../assets/lupas.png" alt="imagem de uma lupa" id="img-lupa" />
-      <input type="text" name="search-input" v-model="name" />
+      <input
+        type="text"
+        name="search-input"
+        v-model="name"
+        placeholder="Digite o nome do Pokemon"
+      />
     </div>
     <button @click="$emit('pesquisarPokemon', name)">pesquisar</button>
   </div>
